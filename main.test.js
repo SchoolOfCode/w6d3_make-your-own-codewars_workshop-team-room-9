a228a875a582055a30d3f733c2407109be7d7eb2
patrick <tasniim_bari@yahoo.com>
2021-12-22 15:24:43 +0000

    step1 completed halfway throguh part 2

--- main.test.js
+++ main.test.js
@@ -1,1 +1,11 @@
 //👉 Write your tests below here:
+
+test('checking your new sentence', ()=>{
+    //arrange
+        const sentence ='Hello there';
+        const expected ='ello here';
+    //act
+        //const actual = function we dont have(sentence)
+    //assert
+        expected(actual).toBe(expected)
+})
